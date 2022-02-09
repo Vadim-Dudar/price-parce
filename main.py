af8bd57738fbf558126fc6c2472579be8a05c3c5
”Vadim <haskervadim228@gmail.com>
2022-02-09 15:20:08 +0200

    innit commim

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
-import config, time, fake_useragent, requests
+import config, time, fake_useragent
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
+from bs4 import  BeautifulSoup as bs
 
 
 options = webdriver.FirefoxOptions()
@@ -10,9 +11,10 @@ driver = webdriver.Firefox(service=Service(executable_path=GeckoDriverManager().
 
 
 try:
-    driver.get('https://instagram.com/h0opsy')
-    time.sleep(12)
-except Exception as ex:
+    driver.get(config.url)
+    soup = bs(driver.page_source, 'lxml')
+    print(soup.find('span', class_='p-card__price--new text-subtitle-price-bold').text.replace('от ', '').replace(' ', '').replace('₽', ''))
+except:
     pass
 finally:
     driver.close()
